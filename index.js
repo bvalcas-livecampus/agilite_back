b63c437c8b5d669c264198b04fcdb55684fee11e
davidelom <davidelom.gnaglo@livecampus.tech>
2025-07-29 10:11:05 +0200

    Add initial backend structure with auth and user model
    Introduces project configuration, database initialization with multiple tables, authentication middleware, and a basic user model. Sets up Express with CORS, body parsing, JWT, and routes for authentication and movies. Updates dependencies to include required packages for authentication and database handling.

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-const express = require("express");
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { openDb, initDb } from "./db.js";
+
+import authRoutes from "./routes/auth.routes.js";
+import movieRoutes from "./routes/movie.routes.js";
+import { JWT_SECRET, PORT } from "./config/config.js";
+
+dotenv.config();
+
 const app = express();
-const port = process.env.PORT || 3000;
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use("/auth", authRoutes);
+app.use("/movies", movieRoutes);
+
+let db;
+await initDb();
+db = await openDb();
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+// ======================
+app.listen(PORT, () => {
+    console.log("Backend Film API listening on port " + PORT);
 });
-module.exports = app;
